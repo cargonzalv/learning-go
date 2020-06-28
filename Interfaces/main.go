4ba1c8c153e5782cccd9a17e4d13371ef4a38da4
Carlos E <cegonzalez@leal.co>
2020-06-28 18:43:53 -0500

    finishing up Udemy tutorial

---
+++ Interfaces/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+type square struct {
+	sideLength float64
+}
+
+func main() {
+
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
